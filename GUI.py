5ad933c68e8dc15af3c39aa4fd69b8f1500cc13f
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2018-09-18 21:26:50 +0300

    Create GUI.py

---
+++ GUI.py
@@ -0,0 +1,41 @@
+####################
+#       GUI        #
+####################
+
+from tkinter import *
+from tkinter import filedialog
+
+def source_folder():
+    window.directory = filedialog.askdirectory(initialdir = "/",title = "Выберите папку с файлами")
+    return window.directory
+
+def dest_folder():
+    window.directory = filedialog.askdirectory(initialdir = "/",title = "Выберите папку с файлами")
+    return window.directory
+
+window=Tk()
+window.geometry("1000x600")
+window.title("ДПК сортировщик")
+
+button_source=Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=source_folder)
+button_source.place(x=40, y=50)
+
+button_dest=Button(window,text="Выберите папку назначения", padx="20", pady="20", command=dest_folder)
+button_dest.place(x=40, y=130)
+
+display=Text(window, width=125, height=35)
+display.place(x=300, y=45)
+
+button_start=Button(window, text="Начать", padx="20", pady="20")
+button_start.place(x=95, y=230)
+
+scroll=Scrollbar(command=display.yview)
+scroll.pack(side=RIGHT, fill=Y)
+display.config(yscrollcommand=scroll.set)
+
+#display.insert(END, "Hello")
+
+
+window.mainloop()
+
+
