f618574f9018a17f1e69cab6f473aa7c062f752a
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2018-09-19 14:21:16 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -13,29 +13,38 @@ def dest_folder():
     window.directory = filedialog.askdirectory(initialdir = "/",title = "Выберите папку с файлами")
     return window.directory
 
-window=Tk()
-window.geometry("1000x600")
+window = Tk()
 window.title("ReSort")
+window.geometry("1000x600")
+window.configure(bg="#f9f9f6")
 
-button_source=Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=source_folder)
+
+
+button_source = Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=source_folder)
 button_source.place(x=40, y=50)
 
-button_dest=Button(window,text="Выберите папку назначения", padx="20", pady="20", command=dest_folder)
+button_dest = Button(window,text="Выберите папку назначения", padx="20", pady="20", command=dest_folder)
 button_dest.place(x=40, y=130)
 
-display=Text(window, width=125, height=35)
-display.place(x=300, y=45)
-
-button_start=Button(window, text="Начать", padx="20", pady="20")
+button_start = Button(window, text="Начать", padx="20", pady="20")
 button_start.place(x=95, y=230)
 
-scroll=Scrollbar(command=display.yview)
-scroll.pack(side=RIGHT, fill=Y)
-display.config(yscrollcommand=scroll.set)
+button_jumptosource = Button(window,text="Перейти", padx="20", pady="10")
+button_jumptosource.place(x=460, y=620)
+
+button_jumptodest = Button(window,text="Перейти", padx="20", pady="10")
+button_jumptodest.place(x=1010, y=620)
+
+display1 = Text(window, width=50, height=35)
+display1.place(x=300, y=50)
+
+display2 = Text(window, width=50, height=35)
+display2.place(x=850, y=50)
+
+arrow = Label(window, text="⇒", bg="#f9f9f6", font=("Times New Roman", 32))
+arrow.place(x=755, y=310)
+
 
 #display.insert(END, "Hello")
 
-
 window.mainloop()
-
-
