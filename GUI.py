bb8b62b3e38462effab416a732f2a0144328911c
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2018-09-21 16:21:06 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -23,8 +23,7 @@ def BothScroll(*args):
     #для одновременного скроллинга двух listboxes
     display1.yview(*args)
     display2.yview(*args)
-    
-    
+       
 
 window = Tk()
 bg_colour="#BDBDBD" #цвет фона
@@ -39,19 +38,19 @@ logo.place(x=40, y=5)
 scrollbar = Scrollbar(window, orient="vertical", command=BothScroll)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-display1 = Listbox(window, width=55, height=25, bd=2, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
+display1 = Listbox(window, width=55, height=25, bd=2, bg="#F5F6CE", font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display1.place(x=300, y=80)
 
-display2 = Listbox(window, width=55, height=25, bd=2, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
+display2 = Listbox(window, width=55, height=25, bd=2, bg="#F5F6CE", font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display2.place(x=850, y=80)
 
 arrow = Label(window, text="⇒", bg=bg_colour, font=("Times New Roman", 32))
 arrow.place(x=775, y=310)
 
-button_source = Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=None)
+button_source = Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=source_folder)
 button_source.place(x=40, y=80)
 
-button_dest = Button(window,text="Выберите папку назначения", padx="20", pady="20", command=None)
+button_dest = Button(window,text="Выберите папку назначения", padx="20", pady="20", command=dest_folder)
 button_dest.place(x=40, y=160)
 
 move_but = Radiobutton(window, text="Переместить", bg=bg_colour)
@@ -59,7 +58,7 @@ copy_but = Radiobutton(window, text="Только копировать", bg=bg_c
 move_but.place(x=50, y=240)
 copy_but.place(x=50, y=270)
 
-button_start = Button(window, text="Начать", padx="20", pady="20", command=lp)
+button_start = Button(window, text="Начать", padx="20", pady="20", command=ReSort)
 button_start.place(x=95, y=320)
 
 button_jumptosource = Button(window,text="Перейти", padx="20", pady="10")
@@ -70,3 +69,4 @@ button_jumptodest.place(x=1025, y=600)
 
 
 window.mainloop()
+
