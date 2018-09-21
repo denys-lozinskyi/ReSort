e2ba5e9507bb9c229896fa43ad2233a0f57261a1
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2018-09-21 16:37:40 +0300

    Update GUI (x.x.2).py

--- GUI (x.x.2).py
+++ GUI (x.x.2).py
@@ -14,37 +14,39 @@ def dest_folder():
     PATH2DEST_FOLDER = (filedialog.askdirectory(initialdir = "\\", title = "Выберите папку с файлами") + "\\")
 
 def lp():
-    #функция для тестирования вывода
+    #функция исключительно для тестирования вывода
     for a in range(100):
         display1.insert(END, (str(a) + " Bring yourself online, Dolores"))
         display2.insert(END, (str(a) + " I am in a dream"))
 
 def BothScroll(*args):
-    #для одновременного скроллинга двух listboxes
+    #обеспечивает одновременный скроллинг листбоксов одним скроллером
     display1.yview(*args)
     display2.yview(*args)
-       
+    
+    
 
 window = Tk()
-bg_colour="#BDBDBD" #цвет фона
-window.title("ReSort")
+general_bg ="#BDBDBD" #цвет общего фона
+displays_bg = "#F5F6CE" #цвет фона дисплеев
+window.title("ReSort build 1.0.2 alpha")
 window.geometry("1000x600")
-window.configure(bg=bg_colour)
+window.configure(bg=general_bg)
 
 
-logo = Label(window, text="ReSort", bg=bg_colour, font=("Brush Script MT", 32))
+logo = Label(window, text="ReSort", bg=general_bg, font=("Brush Script MT", 32))
 logo.place(x=40, y=5)
 
 scrollbar = Scrollbar(window, orient="vertical", command=BothScroll)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-display1 = Listbox(window, width=55, height=25, bd=2, bg="#F5F6CE", font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
+display1 = Listbox(window, width=55, height=25, bd=2, bg=displays_bg, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display1.place(x=300, y=80)
 
-display2 = Listbox(window, width=55, height=25, bd=2, bg="#F5F6CE", font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
+display2 = Listbox(window, width=55, height=25, bd=2, bg=displays_bg, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display2.place(x=850, y=80)
 
-arrow = Label(window, text="⇒", bg=bg_colour, font=("Times New Roman", 32))
+arrow = Label(window, text="⇒", bg=general_bg, font=("Times New Roman", 32))
 arrow.place(x=775, y=310)
 
 button_source = Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=source_folder)
@@ -53,12 +55,12 @@ button_source.place(x=40, y=80)
 button_dest = Button(window,text="Выберите папку назначения", padx="20", pady="20", command=dest_folder)
 button_dest.place(x=40, y=160)
 
-move_but = Radiobutton(window, text="Переместить", bg=bg_colour)
-copy_but = Radiobutton(window, text="Только копировать", bg=bg_colour)
+move_but = Radiobutton(window, text="Переместить", bg=general_bg)
+copy_but = Radiobutton(window, text="Только копировать", bg=general_bg)
 move_but.place(x=50, y=240)
 copy_but.place(x=50, y=270)
 
-button_start = Button(window, text="Начать", padx="20", pady="20", command=ReSort)
+button_start = Button(window, text="Начать", padx="20", pady="20", command=lp)
 button_start.place(x=95, y=320)
 
 button_jumptosource = Button(window,text="Перейти", padx="20", pady="10")
@@ -69,4 +71,5 @@ button_jumptodest.place(x=1025, y=600)
 
 
 window.mainloop()
+
 
