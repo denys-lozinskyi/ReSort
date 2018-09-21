f881d694305aef1370eb818ac2e95f22a02e0c44
Denys Lozinskyi <42441410+DennyL@users.noreply.github.com>
2018-09-21 16:09:20 +0300

    v. x.x.2 alpha
    Text wigets replaced with Lisboxes.
    Scrollbar added. Scrollbar is tied to both listboxes and scrolls them sumultaniousely (which is very convenient if you wanna check a long list of files output in both sections).

--- GUI.py
+++ GUI.py
@@ -15,8 +15,16 @@ def dest_folder():
 
 def lp():
     #функция для тестирования вывода
-    for a in range(30):
-        display1.insert(1.0, "Bring yourself online, Dolores\n")
+    for a in range(100):
+        display1.insert(END, (str(a) + " Bring yourself online, Dolores"))
+        display2.insert(END, (str(a) + " I am in a dream"))
+
+def BothScroll(*args):
+    #для одновременного скроллинга двух listboxes
+    display1.yview(*args)
+    display2.yview(*args)
+    
+    
 
 window = Tk()
 bg_colour="#BDBDBD" #цвет фона
@@ -28,14 +36,17 @@ window.configure(bg=bg_colour)
 logo = Label(window, text="ReSort", bg=bg_colour, font=("Brush Script MT", 32))
 logo.place(x=40, y=5)
 
-display1 = Text(window, width=60, height=25, bd=2, spacing3 = 3, font=("Times New Roman", 12))
+scrollbar = Scrollbar(window, orient="vertical", command=BothScroll)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+display1 = Listbox(window, width=55, height=25, bd=2, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display1.place(x=300, y=80)
 
-display2 = Text(window, width=60, height=25, bd=2, spacing3 = 3, font=("Times New Roman", 12))
+display2 = Listbox(window, width=55, height=25, bd=2, font=("Times New Roman", 12), yscrollcommand=scrollbar.set)
 display2.place(x=850, y=80)
 
 arrow = Label(window, text="⇒", bg=bg_colour, font=("Times New Roman", 32))
-arrow.place(x=795, y=320)
+arrow.place(x=775, y=310)
 
 button_source = Button(window,text="Выберите папку для анализа", padx="20", pady="20", command=None)
 button_source.place(x=40, y=80)
@@ -52,11 +63,10 @@ button_start = Button(window, text="Начать", padx="20", pady="20", command
 button_start.place(x=95, y=320)
 
 button_jumptosource = Button(window,text="Перейти", padx="20", pady="10")
-button_jumptosource.place(x=490, y=650)
+button_jumptosource.place(x=470, y=600)
 
 button_jumptodest = Button(window,text="Перейти", padx="20", pady="10")
-button_jumptodest.place(x=1045, y=650)
+button_jumptodest.place(x=1025, y=600)
 
 
 window.mainloop()
-
